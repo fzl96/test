1684cb15d38eedc7e92be6b77d4d65ba0c35cd29
fzl96 <fpsdragon333@gmail.com>
2023-10-30 08:54:20 +0700

    fix: component not centered

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,11 +7,11 @@ export const metadata = {
 
 export default function LoginPage() {
   return (
-    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] px-10 md:px-0">
+    <div className="h-screen mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] px-10 md:px-0">
       <div className="flex flex-col space-y-2 text-center">
         <Icons.logo className="mx-auto h-7 w-7" />
         <h1 className="text-2xl font-semibold tracking-tight">
-          CV. Family Group
+          Zaid bin Tsabit
         </h1>
         <p className="text-sm text-muted-foreground">
           Masukkan username untuk masuk ke dalam aplikasi
