39cb2ef3504660f1fe1e8baf391071cffb36b6c6
fzl96 <fpsdragon333@gmail.com>
2023-10-30 08:53:52 +0700

    feat: redirect to dashboard when logged in

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
-export default function AuthLayout({ children }: AuthLayoutProps) {
+export default async function AuthLayout({ children }: AuthLayoutProps) {
+  const session = await auth();
+
+  if (session?.user) redirect("/dashboard");
+
   return <div className="min-h-screen">{children}</div>;
 }
