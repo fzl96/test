27b26e06a02e6479eee65b487fe8af89aadadfcc
fzl96 <fpsdragon333@gmail.com>
2023-11-01 03:12:57 +0700

    feat: add pemasukan schema

---
+++ src/lib/validations/pemasukan.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const pemasukanCreateSchema = z.object({
+  keterangan: z.string().optional(),
+  jumlah: z
+    .number({
+      required_error: "Jumlah harus diisi",
+      invalid_type_error: "Jumlah harus berupa angka",
+    })
+    .positive({
+      message: "Jumlah harus lebih dari 0",
+    }),
+});
