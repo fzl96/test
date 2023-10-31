2a684143b6a8a48533a3f259b87b600b6924a96b
fzl96 <fpsdragon333@gmail.com>
2023-11-01 04:04:23 +0700

    fix: rename

--- src/lib/validations/form.ts
+++ src/lib/validations/form.ts
@@ -11,3 +11,15 @@ export const pemasukanCreateSchema = z.object({
       message: "Jumlah harus lebih dari 0",
     }),
 });
+
+export const pengeluaranSchema = z.object({
+  keterangan: z.string().optional(),
+  jumlah: z
+    .number({
+      required_error: "Jumlah harus diisi",
+      invalid_type_error: "Jumlah harus berupa angka",
+    })
+    .positive({
+      message: "Jumlah harus lebih dari 0",
+    }),
+});
