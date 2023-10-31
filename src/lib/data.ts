e754c1923eb932e12a96fa0515c4e790685d29f7
fzl96 <fpsdragon333@gmail.com>
2023-11-01 03:12:33 +0700

    feat: add fetching functions

---
+++ src/lib/data.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { unstable_noStore as noStore } from "next/cache";
+
+const ITEMS_PER_PAGE = 6;
+export async function fetchFilteredPemasukan(currentPage: number) {
+  noStore();
+
+  try {
+    const data = await db.pemasukan.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      skip: (currentPage - 1) * ITEMS_PER_PAGE,
+      take: ITEMS_PER_PAGE,
+    });
+
+    return data;
+  } catch (error) {
+    console.log("Database Error: ", error);
+    throw new Error("Gagal mengambil data pemasukan");
+  }
+}
+
+export async function fetchPemasukanPages() {
+  try {
+    const totalItems = await db.pemasukan.count();
+    const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.log("Database Error: ", error);
+    throw new Error("Gagal mengambil jumlah halaman pemasukan");
+  }
+}
