1a6fc11e7972bc3b3e2e62bcfcb9262c5c9e670b
fzl96 <fpsdragon333@gmail.com>
2023-11-02 08:45:46 +0700

    fix: mobile table

--- src/components/jamaah/jamaah-table.tsx
+++ src/components/jamaah/jamaah-table.tsx
@@ -19,39 +19,39 @@ export async function JamaahTable({
   return (
     <div className="px-5 inline-block min-w-full align-middle">
       <div className="rounded-lg bg-[#f6f8fa] p-2 md:pt-0 shadow-sm">
-        {/* <div className="md:hidden">
+        <div className="md:hidden">
           {jamaah?.map((jmh) => (
-            <div
-              key={jmh.id}
-              className="mb-2 w-full rounded-md bg-white p-4"
-            >
+            <div key={jmh.id} className="mb-2 w-full rounded-md bg-white p-4">
               <div className="flex items-center justify-between border-b pb-4">
                 <div className="text-lg text-gray-500">
-                  <p>{jmh.nama}</p>
-                  <p className="text-lg font-medium">Jumlah: {jmh.jumlah}</p>
+                  <p className="text-gray-800">{jmh.nama}</p>
+                  <p className="text-sm">{jmh.pekerjaan}</p>
                 </div>
               </div>
               <div className="flex w-full items-center justify-between pt-4">
                 <div>
-                  <p className="text-base">{formatDate(jmh.updatedAt)}</p>
+                  <p className="text-sm">{jmh.noHp}</p>
                 </div>
                 <div className="flex justify-end gap-2">
-                  <EditButton
-                    id={jmh.id}
-                    nama={jmh.nama}
-                    jumlah={jmh.jumlah}
-                    keterangan={jmh.keterangan ? jmh.keterangan : ""}
-                  />
+                  <Link
+                    href={`/dashboard/jamaah/${jmh.id}/edit`}
+                    className={cn(
+                      buttonVariants({ variant: "outline" }),
+                      "px-2"
+                    )}
+                  >
+                    <Pencil className="h-4 w-4" />
+                  </Link>
                   <DeleteButton
                     id={jmh.id}
-                    deleteFn={deletejamaah}
                     page="jamaah"
+                    deleteFn={deleteJamaah}
                   />
                 </div>
               </div>
             </div>
           ))}
-        </div> */}
+        </div>
         <table className="hidden min-w-full text-gray-900 md:table">
           <thead className="rounded-lg text-left text-sm font-normal">
             <tr>
