ac0eb2323df5cf3b7068b28f0c4c37cc33d0269d
fzl96 <fpsdragon333@gmail.com>
2024-04-02 15:31:10 +0700

    feat: add loading

---
+++ src/app/(dashboard)/dashboard/pengaturan/loading.tsx
@@ -0,0 +1,10 @@
+import { DashboardHeader } from "@/components/dashboard-header";
+
+export default function Loading() {
+  return (
+    <div className="md:px-5">
+      <DashboardHeader title="Pengaturan" />
+      <div>Loading...</div>
+    </div>
+  );
+}
